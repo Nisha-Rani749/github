4cf4e15df4642efe1ca2991d1c718348c62d323b
Nisha-gif227 <csnisha21@gmail.com>
2021-09-13 03:56:22 +0530

    gitsearch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Layout from "./components/layout/Layout";
+import Dashboard from "./components/Dashboard";
+import User from "./components/User";
+import { GithubProvider } from "./context/GithubContext";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App" style={{ height: "100%" }}>
+        <Layout>
+          <Switch>
+            <GithubProvider>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/user/:username" component={User}></Route>
+              </Switch>
+            </GithubProvider>
+
+            <Route exact path="/about" component={About}></Route>
+            <Route exact path="/contact" component={Contact}></Route>
+          </Switch>
+        </Layout>
+      </div>
+    </Router>
   );
 }
-
-export default App;
